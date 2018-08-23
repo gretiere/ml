1f83a479b519d80971045ac0ad1e5368b8f83061
gretiere <gilles.retiere@free.fr>
2018-08-23 17:42:39 +0200

    .

--- 09/machine-learning-ex8/ex8/cofiCostFunc.m
+++ 09/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = sum(sum( ((X * Theta' - Y) .* R) .^2  )) /2;
 
+X_grad = ((X * Theta' - Y) .* R) * Theta;
 
-
-
-
-
-
-
-
-
-
-
-
-
+Theta_grad = ((X * Theta' - Y) .* R)' * X;
 
 % =============================================================
 
